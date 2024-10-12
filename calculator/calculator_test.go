489a4942809871151e4fbaf6219ab04d95787a69
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-10-12 16:27:25 +0200

    starting with table of test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,11 +5,39 @@ import (
 	"testing"
 )
 
+func TestAdd(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f",
+				tc.a, tc.b, tc.want, got)
+		}
+	}
+}
 func TestSubtract(t *testing.T) {
-	//Arrange	
+	//Arrange
 	t.Parallel()
 	var want float64 = 2
-	got := calculator.Subtract(2, 4)
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 9
+	got := calculator.Multiply(3, 3)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
