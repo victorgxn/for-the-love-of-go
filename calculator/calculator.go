1ddf8aaad548e2f7206e231c41e4eff04b16249e
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-10-17 17:41:11 +0200

    simple sqrt function

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,6 +1,10 @@
 package calculator
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 func Add(a, b float64) float64 {
 	return a + b
@@ -19,4 +23,11 @@ func Divide(a, b float64) (float64, error) {
 		return 0, errors.New("division by zero not allowed")
 	}
 	return a / b, nil
+}
+
+func Sqrt(input float64) (float64, error) {
+	if input < 0 {
+		return 0, fmt.Errorf("square root of negative number not allowed: %f", input)
+	}
+	return math.Sqrt(input), nil
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -1,9 +1,9 @@
 package calculator_test
 
 import (
-	"calculator"
 	"math"
 	"testing"
+	"calculator"
 )
 
 func TestAdd(t *testing.T) {
@@ -73,14 +73,15 @@ func TestDivide(t *testing.T) {
 		want float64
 	}
 	testCases := []testCase{
-		{a: 1, b: 3, want: 0.333333},
+		{a: 2, b: 2, want: 1},
 		{a: -1, b: -1, want: 1},
 		{a: 10, b: 2, want: 5},
+		{a: 1, b: 3, want: 0.333333},
 	}
 	for _, tc := range testCases {
 		got, err := calculator.Divide(tc.a, tc.b)
 		if err != nil {
-			t.Fatalf("want no error for valid input, got %v", err)
+			t.Fatalf("Divide(%f, %f): want no error for valid input, got %v", tc.a, tc.b, err)
 		}
 		if !closeEnough(tc.want, got, 0.001) {
 			t.Errorf("Divide(%f, %f): want %f, got %f",
@@ -95,7 +96,38 @@ func TestDivideInvalid(t *testing.T) {
 	if err == nil {
 		t.Error("want error for invalid input, got nil")
 	}
+}
 
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		input float64
+		want  float64
+	}
+	testCases := []testCase{
+		{input: 4, want: 2},
+		{input: 2, want: 1.41421356237},
+		{input: 25, want: 5},
+		{input: 1.4, want: 1.2},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.input)
+		if err != nil {
+			t.Fatalf("Sqrt(%f): want no error for valid input, got %v", tc.input, err)
+		}
+		if !closeEnough(tc.want, got, 0.1) {
+			t.Errorf("Sqrt(%f): want %f, got %f",
+				tc.input, tc.want, got)
+		}
+	}
+}
+
+func TestSqrtInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := calculator.Sqrt(-1)
+	if err == nil {
+		t.Error("Sqrt(-1): want error for invalid input, got nil")
+	}
 }
 
 func closeEnough(a, b, tolerance float64) bool {
