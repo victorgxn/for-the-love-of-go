a6145d0dac7566699e6fbb9b834c51e7d236d5e2
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-10-23 20:45:40 +0200

    types, as default type numbers start with 0 string with  and bool in false

---
+++ pag50/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var title string
+	var copies int
+	title = "For the Love of Go"
+	copies = 99
+	var version float32 = 1.22
+	x := 2
+	fmt.Println(x)
+	fmt.Println(title)
+	fmt.Println(copies)
+	fmt.Println(version)
+}
